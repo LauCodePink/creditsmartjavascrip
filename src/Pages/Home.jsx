b193f6eef618f13bc704d7111d4134c7e6a8efb4
julianespinosa12-arte <julian.espinosa12@est.iudigital.edu.co>
2025-11-28 00:07:09 -0500

    creacion paginas

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,23 @@
+// src/pages/Home.jsx
+import React from 'react';
+import Hero from '../Components/Hero';
+import CreditCard from '../Components/CreditCard';
+import { creditOffers } from '../data/creditsData';
+
+const Home = () => {
+  return (
+    <main className="container">
+      <Hero />
+      <section className="credits-section">
+        <h3>CREDITOS QUE TE OFRECEMOS</h3>
+        <div className="credits-grid">
+          {creditOffers.map(credit => (
+            <CreditCard key={credit.id} credit={credit} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
