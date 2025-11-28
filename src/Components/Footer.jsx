40c823f8ca21fc89f01e57cb834a55e9e775db3a
julianespinosa12-arte <julian.espinosa12@est.iudigital.edu.co>
2025-11-27 23:12:12 -0500

    finalalizacion de componentes

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,38 @@
+/ src/components/Footer.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <div className="footer-info">
+            <img src="src\images\IMG.png" alt="CredySmartJDL" className="footer-logo" />
+            <p>Confianza, transparencia y soluciones financieras a tu medida.</p>
+          </div>
+          <div className="footer-links">
+            <h4>Enlaces</h4>
+            <ul>
+              <li><Link to="/">Inicio</Link></li>
+              <li><Link to="/simulador">Simulador</Link></li>
+              <li><Link to="/solicitud">Solicitar Crédito</Link></li>
+              <li><a href="#">Términos y Condiciones</a></li>
+            </ul>
+          </div>
+          <div className="footer-contact">
+            <h4>Contacto</h4>
+            <p>📞 +57 300 123 4567</p>
+            <p>✉️ contacto@credysmartjdl.com</p>
+            <p>📍 Medellin, Colombia</p>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>&copy; 2025 CredySmartJDL. Todos los derechos reservados.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,19 @@
+// src/components/Hero.jsx
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <header className="hero">
+      <div className="container">
+        <h2>Encuentra tu credito perfecto</h2>
+        <p>
+          ¿Buscas financiar tus metas? En CreditSmart tenemos la solución ideal para ti. Explora nuestros
+          productos, compara tasas y plazos, y encuentra el crédito que se adapte a tu necesidad. ¡Todo en unos
+          pocos clics!
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
